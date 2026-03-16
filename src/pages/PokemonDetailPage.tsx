374312ab33c62cc53a20fab3708479135cf75efa
thomas-lefebvre <t.lefebvre.div@gmail.com>
2026-03-16 22:35:54 +0100

    - remove debug

--- src/pages/PokemonDetailPage.tsx
+++ src/pages/PokemonDetailPage.tsx
@@ -11,8 +11,6 @@ function PokemonDetailPage() {
   const { pokemon, error } = usePokemon(id!, state?.pokemon ?? null);
   const previousGridPage = state.previousGridPage ?? 1;
 
-  console.log(state.previousGridPage);
-
   if (error)
     return <div className="min-h-screen bg-slate-900 text-red-400 flex items-center justify-center">{error}</div>;
 
